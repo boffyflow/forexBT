fd9e434d2ec47917371a4bb179a7bc06859f99e0
boffyflow <boffyflow@gmail.com>
2017-11-19 21:46:42 -0800

    safety checkin

--- util/builddb.py
+++ util/builddb.py
@@ -1,26 +1,17 @@
 import sqlite3
 import argparse
-
-
-def addcurrency(conn, currency):
-    """ add a list of currency strings to be added to database"""
-
-    cur = conn.cursor()
-
-    for c in currency:
-        print(c)
-        cur.execute('INSERT INTO currencies(name) VALUES (?)', (c, ))
-
-    conn.commit()
+import pandas as pd
 
 
 def createtables(conn):
-    
-    # Connecting to the database file
-    c = conn.cursor()
 
-    # Creating a new SQLite table with 1 column
-    c.execute('CREATE TABLE IF NOT EXISTS trades (id INTEGER PRIMARY KEY ASC,\
+    # Connecting to the database file
+
+    cur = conn.cursor()
+
+    # Create the tables
+
+    cur.execute('CREATE TABLE IF NOT EXISTS trades (id INTEGER PRIMARY KEY ASC,\
                                                     date_open DATETIME,\
                                                     date_close DATETIME,\
                                                     price_open DOUBLE,\
@@ -29,19 +20,37 @@ def createtables(conn):
                                                     volume INT,\
                                                     stop_orig DOUBLE,\
                                                     tp_orig DOUBLE,\
-                                                    currency_id INT)')
+                                                    currency_id INT,\
+                                                    open_type_id INT,\
+                                                    close_type_id INT)')
 
-    c.execute('CREATE TABLE IF NOT EXISTS currencies (id INTEGER PRIMARY KEY ASC,\
-                                                    name VARCHAR(10))')
+    cur.execute('CREATE TABLE IF NOT EXISTS currencies (id INTEGER PRIMARY KEY ASC,\
+                                                    name VARCHAR(10),\
+                                                    UNIQUE(name))')
 
-    c.execute('CREATE TABLE IF NOT EXISTS accounts (id INTEGER PRIMARY KEY,\
+    cur.execute('CREATE TABLE IF NOT EXISTS accounts (id INTEGER PRIMARY KEY,\
                                     date_open DATETIME,\
                                     date_close DATETIME)')
 
-    c.execute('CREATE TABLE IF NOT EXISTS strategies (id INTEGER PRIMARY KEY,\
+    cur.execute('CREATE TABLE IF NOT EXISTS strategies (id INTEGER PRIMARY KEY,\
                                     date_open DATETIME,\
                                     date_close DATETIME)')
-    
+
+    cur.execute('CREATE TABLE IF NOT EXISTS ordertypes (id INTEGER PRIMARY KEY,\
+                                    name VARCHAR(32))')
+
+    conn.commit()
+
+    # adding reasonable default entries to the tables
+
+    currencies = ['USD', 'CAD', 'EUR', 'JPY', 'AUD', 'CHF', 'NZD', 'GBP']
+    for c in currencies:
+        cur.execute('INSERT INTO currencies(name) VALUES (?)', (c, ))
+
+    otypes = ['buy_limit', 'sell_limit', 'buy_market', 'sell_market', 'buy_stop', 'sell_stop']
+    for o in otypes:
+        cur.execute('INSERT INTO ordertypes(name) VALUES (?)', (o, ))
+
     conn.commit()
 
 def parse_args():
@@ -58,8 +67,13 @@ def main():
     
     conn = sqlite3.connect(args.dbname)
 
-    createtables(conn)
-    addcurrency(conn, ['USD','CAD','EUR','JPY','AUD','CHF','NZD','GBP'])
+#    createtables(conn)
+
+    # print out table test
+    df = pd.read_sql_query("SELECT * from currencies", conn)
+
+    # verify that result of SQL query is stored in the dataframe
+    print(df)
 
     conn.close()
 
